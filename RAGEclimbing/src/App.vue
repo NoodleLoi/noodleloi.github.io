f82fea49555c70e8b383ba874f88f295d8edd879
Loi <lduongjr@gmail.com>
2022-11-16 12:29:40 -0500

    Homepage img idk what to do

--- RAGEclimbing/src/App.vue
+++ RAGEclimbing/src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from './components/NavBar.vue'
+import FrontPage from './components/FrontPage.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <NavBar/>
+    <div class="background">
+      <img src='./assets/Paddle.JPEG'>
+    </div>
+    <div>
+      <h3>Hello World!</h3>
+    </div>
   </main>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  .background img{
+    width: 100px;
+    
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
